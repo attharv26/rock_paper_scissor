55eef8130a2bca9ba954e160e20f3f1561c6fe52
attharv26 <voltpanda4511@gmail.com>
2024-06-20 23:15:31 +0530

    update

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,10 +1,36 @@
-from random import choice
+# Author: Atharv Pandey
+# Date  : 20th JUNE, 2024
+from random import randint
 def winner(user, comp):
     if user == comp:
-        print(r'It's a tie' )
-        return false
+        return None
     elif(user == 1):
-        
-arr = {1: 'rock🪨', 2: 'paper📃', 3: 'scissor✂️'}
-print(choice(list(arr.values())))
+        if(comp == 2):return False
+        elif(comp == 3):return True
+    elif(user == 2):
+        if(comp == 1):return True
+        elif(comp == 3):return False
+    elif(user == 3):
+        if(comp == 1):return False
+        elif(comp == 2):return True
 
+arr = {1: 'Rock🪨', 2: 'Paper📃', 3: 'Scissor✂️'}
+user_score = 0
+comp_score = 0
+for i in range(0,3):
+    try:
+        user = int(input('Press\n1: Rock🪨 \n2: Paper📃 \n3: Scissors✂️\n\n\n'))
+        comp = randint(1,3)
+        print(f'YOU     :{arr[user]}\nCOMPUTER:{arr[comp]}  \n')
+        if winner(user, comp):
+            user_score += 1
+        else:
+            comp_score += 1
+    except:
+        print('INVALID CHOICE')
+if user_score > comp_score:
+    print("YOU WIN!")
+elif comp_score > user_score:
+    print("🖥️ WINS!")
+elif user_score == comp_score:
+    print('its a tie!'.upper())
