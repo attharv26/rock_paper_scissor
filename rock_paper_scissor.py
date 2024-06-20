bf44a94748740f26bfe50d499a7184c2d4897de4
attharv26 <voltpanda4511@gmail.com>
2024-06-20 23:11:52 +0530

    first commit

---
+++ rock_paper_scissor.py
@@ -0,0 +1,10 @@
+from random import choice
+def winner(user, comp):
+    if user == comp:
+        print(r'It's a tie' )
+        return false
+    elif(user == 1):
+        
+arr = {1: 'rock🪨', 2: 'paper📃', 3: 'scissor✂️'}
+print(choice(list(arr.values())))
+
